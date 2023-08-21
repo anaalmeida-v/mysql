a660c67ff3d61325a11b8d8ac1f60decca1f1ed1
aninhaalmeidav <aninhaalmeidav@gmail.com>
2023-08-21 17:43:45 -0300

    teste

---
+++ phpMyAdmin.php
@@ -0,0 +1,8 @@
+/* Trecho de configurações do phpMyAdmin */
+
+/* Cole isso em seu config.inc.php */
+
+$cfg['Console']['Mode'] = 'show';
+$cfg['lang'] = 'pt_BR';
+$cfg['Console']['Height'] = 145.85589000000004;
+$cfg['NavigationWidth'] = 0;
